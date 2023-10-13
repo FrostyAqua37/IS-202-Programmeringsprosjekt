0d662b30b5017afed6c2066a9ae843df81e7e9dd
FrostyAqua37 <eivindc@uia.no>
2023-10-13 23:10:55 +0200

    Gjorde ferdigt sidebar og satte tittel på alle sidene

---
+++ Controllers/WorkScheduleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nøsted.Controllers
+{
+    public class WorkScheduleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
