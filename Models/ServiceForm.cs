52cb60790b0f575d585ca9ac1ae9fb62799c896a
krisfollegg <kris.follegg@gmail.com>
2023-10-16 19:27:34 +0200

    guphhuj

---
+++ Models/ServiceForm.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nøsted.Models
+{
+    public class ServiceForm
+    {
+        public ServiceForm()
+        {
+            
+        }
+
+        [Key]
+        public int OrderNr { get; set; }
+
+        public int ServiceNr { get; set; }
+
+        public string Modell { get; set; }
+
+        public string Kommentar { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
