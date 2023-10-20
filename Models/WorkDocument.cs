a40a77c39bd0bb18c09a9cca832ae9a8f59ef03b
krisfollegg <kris.follegg@gmail.com>
2023-10-20 18:58:46 +0200

    Lagt til arbeidsdokument

---
+++ Models/WorkDocument.cs
@@ -0,0 +1,21 @@
+﻿namespace NøstedProsjekt.Models
+{
+    public class WorkDocument
+    {
+        //Arbeidsdokument
+        public int OrderId { get; set; }
+        public string ProductType { get; set; } = string.Empty;
+        public string Comment { get; set; } = string.Empty;
+        public string Week { get; set; } = string.Empty;
+        public string Inquiry { get; set; } = string.Empty;
+        public bool CaseCompleted { get; set; }
+        public string CustomerInfo { get; set; } = string.Empty;
+        public string PlannedDelivery { get; set; } = string.Empty;
+        public DateTime? ProductReceivedDate { get; set; }
+        public DateTime? AgreedCompletionDate { get; set; }
+        public DateTime? ServiceCompletedDate { get; set; }
+        public string ServiceHours { get; set; } = string.Empty;
+        public bool HasOrderNumber { get; set; }
+        public bool HasServiceForm { get; set; }
+    }
+}
