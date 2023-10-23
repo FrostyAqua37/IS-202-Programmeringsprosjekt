4783969a185e011ae9ba6dda8b8fe3af1e9d5865
krisfollegg <kris.follegg@gmail.com>
2023-10-23 17:48:08 +0200

    Endret serviceskjema oppsett

--- Models/ServiceOrder.cs
+++ Models/ServiceOrder.cs
@@ -3,19 +3,23 @@
     public class ServiceOrder
     {
         public int Id { get; set; }
-
-        // Customer Information
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public string Customer { get; set; } = string.Empty;
+        public string RecivedDate { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string EmailAddress { get; set; } = string.Empty;
-
-        // Product Details
-        public string ProductName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string YearModell { get; set; } = string.Empty;
         public string ProductType { get; set; } = string.Empty;
-
-        // Service Details
-        public string PreferredTimePeriod { get; set; } = string.Empty;
-        public string Comment { get; set; } = string.Empty;
+        public string Insurance { get; set; } = string.Empty;
+        public string SerialNumber { get; set; } = string.Empty;
+        public string WhatIsPlanned { get; set; } = string.Empty;
+        public string OperationDescription { get; set; } = string.Empty;
+        public string UsedParts { get; set; } = string.Empty;
+        public string WorkHours { get; set; } = string.Empty;
+        public string FinishedDate { get; set; } = string.Empty;
+        public string UsedPartsReturned { get; set; } = string.Empty;
+        public string ShippingMethod { get; set; } = string.Empty;
+        public string SignCustomer { get; set; } = string.Empty;
+        public string SignMechanic { get; set; } = string.Empty;
     }
 }
