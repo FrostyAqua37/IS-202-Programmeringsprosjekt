8b425d9161a8edee93e3e5c9b5eb340a5e9c1b38
FrostyAqua37 <eivindc@uia.no>
2023-10-25 15:29:41 +0200

    Endringer til ServiceOrderController

--- Controllers/ServiceOrderController.cs
+++ Controllers/ServiceOrderController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NøstedProsjekt.Models;
 using System.Collections.Generic;
+using System.Web;
 
 namespace Nøsted.Controllers
 {
+    [AutoValidateAntiforgeryToken]
     public class ServiceOrderController : Controller
     {
         public static List<ServiceOrder> serviceOrder = new List<ServiceOrder>();
@@ -34,14 +36,14 @@ namespace Nøsted.Controllers
         {
             ServiceOrder serviceOrder = new ServiceOrder();
             serviceOrder.OrderId = OrderId;
-            serviceOrder.FirstName = FirstName;
-            serviceOrder.LastName = LastName;
-            serviceOrder.PhoneNumber = PhoneNumber;
-            serviceOrder.Email = Email;
-            serviceOrder.ProductName = ProductName;
-            serviceOrder.ProductType = ProductType;
-            serviceOrder.PreferredTimePeriod = PreferredTimePeriod;
-            serviceOrder.Comment = Comment;
+            serviceOrder.FirstName = HttpUtility.HtmlEncode(FirstName);
+            serviceOrder.LastName = HttpUtility.HtmlEncode(LastName); ;
+            serviceOrder.PhoneNumber = HttpUtility.HtmlEncode(PhoneNumber); 
+            serviceOrder.Email = HttpUtility.HtmlEncode(Email);
+            serviceOrder.ProductName = HttpUtility.HtmlEncode(ProductName);
+            serviceOrder.ProductType = HttpUtility.HtmlEncode(ProductType);
+            serviceOrder.PreferredTimePeriod = HttpUtility.HtmlEncode(PreferredTimePeriod);
+            serviceOrder.Comment = HttpUtility.HtmlEncode(Comment);
 
             return View("Overview");
         }
@@ -51,18 +53,18 @@ namespace Nøsted.Controllers
         public IActionResult CreateWorkDocument(string ProductType, string Comment, string Order, string Week, string Inquiry, bool CaseCompleted, string CustomerInfo, DateTime? PlannedDelivery, DateTime? ProductReceivedDate, DateTime? AgreedCompletionDate, DateTime? ServiceCompletedDate, string ServiceHours, bool HasOrderNumber, bool HasServiceForm)
         {
             WorkDocument workDocument = new WorkDocument();
-            workDocument.ProductType = ProductType; 
-            workDocument.Comment = Comment;
-            workDocument.Order = Order;
-            workDocument.Week = Week;
-            workDocument.Inquiry = Inquiry;
+            workDocument.ProductType = HttpUtility.HtmlEncode(ProductType); 
+            workDocument.Comment = HttpUtility.HtmlEncode(Comment);
+            workDocument.Order = HttpUtility.HtmlEncode(Order);
+            workDocument.Week = HttpUtility.HtmlEncode(Week);
+            workDocument.Inquiry = HttpUtility.HtmlEncode(Inquiry);
             workDocument.CaseCompleted = CaseCompleted;
-            workDocument.CustomerInfo = CustomerInfo;
+            workDocument.CustomerInfo = HttpUtility.HtmlEncode(CustomerInfo);
             workDocument.PlannedDelivery = PlannedDelivery;
             workDocument.ProductReceivedDate = ProductReceivedDate;
             workDocument.AgreedCompletionDate = AgreedCompletionDate;
             workDocument.ServiceCompletedDate = ServiceCompletedDate;
-            workDocument.ServiceHours = ServiceHours;
+            workDocument.ServiceHours = HttpUtility.HtmlEncode(ServiceHours);
             workDocument.HasOrderNumber = HasOrderNumber;
             workDocument.HasServiceForm = HasServiceForm;
 
